88176668eb923bd7e8d61d37e8e96e4dcecd0b6c
Berdiguly Yaylymov <yaylymovberdi@gmail.com>
2024-12-01 18:59:17 +0100

    First day solution to both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,30 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { line ->
+            val first = line.substringBefore("  ").toInt()
+            val second = line.substringAfterLast("  ").toInt()
+            first to second
+        }.unzip()
+
+        val sortedLeft = left.sorted()
+        val sortedRight = right.sorted()
+
+        val result = sortedLeft.zip(sortedRight).sumOf { (left, right) -> kotlin.math.abs(left - right) }
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (left, right) = input.map { line ->
+            line.split(Regex("\\s+")).let {
+                it[0].toLong() to it[1].toLong()
+            }
+        }.unzip()
+
+        return left.fold(0L) { acc, num ->
+            acc + num * right.groupingBy { it }.eachCount().getOrDefault(num, 0)
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,12 @@
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
-import kotlin.io.path.readText
+import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
